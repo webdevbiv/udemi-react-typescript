1b901b5b0a7a95133ba9ffaf8ae71fd96727055e
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-01-29 15:40:30 +0200

    ADD | handleAddGoal

--- module-03-02/react-ts-basics/src/App.tsx
+++ module-03-02/react-ts-basics/src/App.tsx
@@ -1,13 +1,34 @@
+import {useState} from "react";
+
 import {CourseGoal} from "./components";
 import {Header} from "./components";
 import goalsImg from "./assets/goals.jpg";
 
+type CourseGoal = {
+  id: number;
+  title: string;
+  description: string;
+};
+
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+  function handleAddGoal() {
+    setGoals((prev) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: "Learn React + TS",
+        description: "Learn it in depth",
+      };
+
+      return [...prev, newGoal];
+    });
+  }
   return (
     <main>
       <Header image={{src: goalsImg, alt: "A list of goals"}}>
         <h1>Your Course Goals</h1>
       </Header>
+      <button onClick={handleAddGoal}>Add Goal</button>
       <CourseGoal
         title='Learn React + TS'
         // description='Learn it from ground up'
