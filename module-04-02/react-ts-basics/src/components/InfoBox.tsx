bde7f86f4889c3c850dcb89c463601b47968c65a
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-01-30 10:02:42 +0200

    UPD | InfoBox

--- module-04-02/react-ts-basics/src/components/InfoBox.tsx
+++ module-04-02/react-ts-basics/src/components/InfoBox.tsx
@@ -1,13 +1,21 @@
 import {ReactNode} from "react";
 
-type InfoBoxProps = {
-  mode: "hint" | "warning";
-  severity?: "low" | "medium" | "high";
+type HintBoxProps = {
+  mode: "hint";
   children: ReactNode;
 };
 
-function InfoBox({mode, severity, children}: InfoBoxProps) {
-  if (mode === "hint") {
+type WarningBoxProps = {
+  mode: "warning";
+  severity: "low" | "medium" | "high";
+  children: ReactNode;
+};
+
+type InfoBoxProps = HintBoxProps | WarningBoxProps;
+
+function InfoBox(props: InfoBoxProps) {
+  const {children, mode} = props;
+  if (props.mode === "hint") {
     return (
       <aside className='infobox infobox-hint'>
         <p>{children}</p>
@@ -15,6 +23,7 @@ function InfoBox({mode, severity, children}: InfoBoxProps) {
     );
   }
 
+  const {severity} = props;
   return (
     <aside className={`infobox infobox-warning warning--${severity}`}>
       {mode === "warning" ? <h2>Warning</h2> : null}
