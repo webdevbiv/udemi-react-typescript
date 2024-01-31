5b464b031a07f6a7ce63b62ea3c27cffd3c96c4f
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-01-31 20:06:19 +0200

    UPD | type Predicate

--- module-04-02/04 New Project Starting Code/src/App.tsx
+++ module-04-02/04 New Project Starting Code/src/App.tsx
@@ -1,18 +1,14 @@
 import Button from "./components/Button";
-import Input from "./components/Input";
+// import Input from "./components/Input";
 
 function App() {
   return (
     <main>
       <p>
-        <Button el='button'>Button</Button>
+        <Button>Button</Button>
       </p>
       <p>
-        <Button
-          el='a'
-          href='https://google.com'>
-          Link
-        </Button>
+        <Button href='https://google.com'>Link</Button>
       </p>
     </main>
   );
--- module-04-02/04 New Project Starting Code/src/components/Button.tsx
+++ module-04-02/04 New Project Starting Code/src/components/Button.tsx
@@ -5,36 +5,44 @@ type BaseProps = {
   className?: string;
 };
 
-type ButtonProps = BaseProps & {
-  el: "button";
-} & Omit<ComponentPropsWithoutRef<"button">, keyof BaseProps>;
+// type ButtonProps = BaseProps & {
+//   el: "button";
+// } & Omit<ComponentPropsWithoutRef<"button">, keyof BaseProps>;
 
-type AnchorProps = BaseProps & {
-  el: "a";
-  href: string;
-} & Omit<ComponentPropsWithoutRef<"a">, keyof BaseProps>;
+// type AnchorProps = BaseProps & {
+//   el: "a";
+//   href: string;
+// } & Omit<ComponentPropsWithoutRef<"a">, keyof BaseProps>;
+
+type ButtonProps = ComponentPropsWithoutRef<"button"> & {
+  href?: never;
+};
+
+type AnchorProps = ComponentPropsWithoutRef<"a"> & {
+  href?: string;
+};
+
+//NOTE - Type Predicate
+function isAnchorProps(props: ButtonProps | AnchorProps): props is AnchorProps {
+  return "href" in props;
+}
 
 type ButtonComponentProps = ButtonProps | AnchorProps;
 
 function Button(props: ButtonComponentProps) {
-  const {el, text, className = "button", ...restProps} = props;
-
-  if ("href" in props) {
+  // if ("href" in props)
+  if (isAnchorProps(props)) {
     return (
       <a
-        {...(restProps as ComponentPropsWithoutRef<"a">)}
-        className={className}>
-        {text}
-      </a>
+        {...props}
+        className='button'></a>
     );
   }
 
   return (
     <button
-      {...(restProps as ComponentPropsWithoutRef<"button">)}
-      className={className}>
-      {text}
-    </button>
+      {...props}
+      className='button'></button>
   );
 }
 
