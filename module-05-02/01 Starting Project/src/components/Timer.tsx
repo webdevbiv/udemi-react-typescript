865489e70de791bc72069f551a9abf621c12a440
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-03-12 17:30:39 +0200

    ADD | useEffect Timer

--- module-05-02/01 Starting Project/src/components/Timer.tsx
+++ module-05-02/01 Starting Project/src/components/Timer.tsx
@@ -1,17 +1,24 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {type Timer as TimerProps} from "../context/timers-context.tsx";
 import Container from "./UI/Container.tsx";
 
 export default function Timer({name, duration}: TimerProps) {
+  const interval = useRef<number | null>(null);
   const [timeLeft, setTimeLeft] = useState(duration * 1000);
 
+  if (timeLeft <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
   useEffect(() => {
-    if (timeLeft > 0) {
-      setTimeout(() => {
-        setTimeLeft(timeLeft - 50);
-      }, 50);
-    }
-  });
+    const timer = (interval.current = setInterval(() => {
+      setTimeLeft((prevTime) => prevTime - 50);
+    }, 50));
+    interval.current = timer;
+    console.log(interval.current);
+
+    return () => clearInterval(timer);
+  }, []);
 
   const formattedTimeLeft = (timeLeft / 1000).toFixed(2);
 
