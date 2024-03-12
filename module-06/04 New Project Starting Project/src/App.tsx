b61e89e8e2f88fcbfa620b29a8ac484ca0fba952
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-03-12 19:05:17 +0200

    ADD | App fetch

--- module-06/04 New Project Starting Project/src/App.tsx
+++ module-06/04 New Project Starting Project/src/App.tsx
@@ -1,5 +1,51 @@
+import {ReactNode, useEffect, useState} from "react";
+import {get} from "./util/http";
+import BlogPosts, {type BlogPost} from "./components/BlogPosts";
+import fetchingImg from "./assets/data-fetching.png";
+
+type RawBlogPosts = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[] | undefined>([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const posts = (await get(
+        "https://jsonplaceholder.typicode.com/posts"
+      )) as RawBlogPosts[];
+
+      const blogPosts: BlogPost[] = posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+        text: post.body,
+      }));
+
+      setFetchedPosts(blogPosts);
+    }
+
+    fetchPosts();
+  });
+
+  let content: ReactNode;
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  return (
+    <main>
+      <img
+        src={fetchingImg}
+        alt='Fetching image depicting data fetching'
+      />
+      {content}
+    </main>
+  );
 }
 
 export default App;
