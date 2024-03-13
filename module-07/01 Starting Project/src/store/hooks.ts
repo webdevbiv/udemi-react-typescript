7b213634b9dc2da977979b11b223eaf0ad09964c
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-03-13 19:32:23 +0200

    ADD | cartQuantity

--- module-07/01 Starting Project/src/store/hooks.ts
+++ module-07/01 Starting Project/src/store/hooks.ts
@@ -3,9 +3,9 @@ import {
 	useSelector,
 	type TypedUseSelectorHook
 } from 'react-redux';
-import { AppDispatch } from './store';
+import { AppDispatch, RootState } from './store';
 
 type DispatchFunction = () => AppDispatch;
 
 export const useCartDispatch: DispatchFunction = useDispatch;
-export const useCartSelector: TypedUseSelectorHook = useSelector;
+export const useCartSelector: TypedUseSelectorHook<RootState> = useSelector;
--- module-07/01 Starting Project/src/store/store.ts
+++ module-07/01 Starting Project/src/store/store.ts
@@ -8,5 +8,5 @@ export const store = configureStore({
 	}
 });
 
-export type RootState = typeof store.getState;
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
