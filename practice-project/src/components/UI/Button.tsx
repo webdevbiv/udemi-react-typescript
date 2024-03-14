9e97e9bcacac54d972b8506c1e04da1197e8001e
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-03-14 17:54:26 +0200

    UPD | Button

--- practice-project/src/components/UI/Button.tsx
+++ practice-project/src/components/UI/Button.tsx
@@ -1,28 +1,51 @@
+import React from 'react';
 import { Link, LinkProps } from 'react-router-dom';
 
-type ButtonProps = LinkProps &
-	React.ButtonHTMLAttributes<HTMLButtonElement> & {
-		text: string | 'Click me';
-		textOnly?: boolean;
+type BaseProps = {
+	children?: React.ReactNode;
+	textOnly?: boolean;
+};
+
+type ButtonLinkProps = LinkProps & BaseProps;
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+	BaseProps & {
+		to?: never;
 	};
 
-const Button = ({ to, text, textOnly, ...rest }: ButtonProps) => {
-	const className = 'button ' + (textOnly ? 'text-only' : '');
+// Type guard function to check if props are for ButtonLinkProps
+function isLink(
+	props: ButtonProps | ButtonLinkProps
+): props is ButtonLinkProps {
+	return 'to' in props;
+}
 
-	return to ? (
-		<Link
-			to={to}
-			className={className}
-			{...rest}>
-			{text}
-		</Link>
-	) : (
-		<button
-			className={className}
-			{...rest}>
-			{text}
-		</button>
-	);
+const Button = ({ textOnly, ...props }: ButtonProps | ButtonLinkProps) => {
+	const className = `button ${textOnly ? 'text-only' : ''}`;
+
+	if (isLink(props)) {
+		// Props specific to <Link>
+		const { children, to, ...rest } = props;
+		return (
+			<Link
+				to={to}
+				className={className}
+				{...rest}>
+				{children}
+			</Link>
+		);
+	} else {
+		// Props specific to <button>
+		// Since ButtonProps includes all valid <button> attributes, you can spread them directly
+		const { children, ...rest } = props;
+		return (
+			<button
+				className={className}
+				{...rest}>
+				{children}
+			</button>
+		);
+	}
 };
 
 export default Button;
