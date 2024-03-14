b23f2edc54eebeaed0a0ed55ba74528193d99aca
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-03-14 13:45:11 +0200

    ADD | dispatch to CartItems

--- module-07/01 Starting Project/src/components/CartItems.tsx
+++ module-07/01 Starting Project/src/components/CartItems.tsx
@@ -1,9 +1,18 @@
-import { useCartSelector } from '../store/hooks';
+import { CartItem, addToCart } from '../store/cart-slice';
+import { useCartSelector, useCartDispatch } from '../store/hooks';
 import { getCartItems } from '../store/selectors';
 
 export default function CartItems() {
+	const dispatch = useCartDispatch();
 	const cartItems = useCartSelector(getCartItems);
-	console.log(cartItems);
+
+	const formattedTotalPrice = `$${cartItems
+		.reduce((total, item) => total + item.price * item.quantity, 0)
+		.toFixed(2)}`;
+
+	function handleAddToCart(item: CartItem) {
+		dispatch(addToCart(item));
+	}
 
 	return (
 		<div id='cart'>
@@ -24,16 +33,16 @@ export default function CartItems() {
                     -
                   </button> */}
 								<span>{item.quantity}</span>
-								{/* <button onClick={() => handleAddToCart(item)}>+</button> */}
+								<button onClick={() => handleAddToCart(item)}>+</button>
 							</div>
 						</li>
 					);
 				})}
 			</ul>
 
-			{/* <p id="cart-total-price">
-        Cart Total: <strong>{formattedTotalPrice}</strong>
-      </p> */}
+			<p id='cart-total-price'>
+				Cart Total: <strong>{formattedTotalPrice}</strong>
+			</p>
 		</div>
 	);
 }
