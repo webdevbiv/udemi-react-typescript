4374c5dc77ddf9e37cee467cce3340b81c33612a
Igor <80968651+webdevbiv@users.noreply.github.com>
2024-03-14 11:31:58 +0200

    ADD | CartItems

--- module-07/01 Starting Project/src/components/CartItems.tsx
+++ module-07/01 Starting Project/src/components/CartItems.tsx
@@ -9,27 +9,27 @@ export default function CartItems() {
 		<div id='cart'>
 			<p>No items in cart!</p>
 
-			{/* <ul id="cart-items">
-          {cartItems.map((item) => {
-            const formattedPrice = `$${item.price.toFixed(2)}`;
+			<ul id='cart-items'>
+				{cartItems.map((item) => {
+					const formattedPrice = `$${item.price.toFixed(2)}`;
 
-            return (
-              <li key={item.id}>
-                <div>
-                  <span>{item.title}</span>
-                  <span> ({formattedPrice})</span>
-                </div>
-                <div className="cart-item-actions">
-                  <button onClick={() => handleRemoveFromCart(item.id)}>
+					return (
+						<li key={item.id}>
+							<div>
+								<span>{item.title}</span>
+								<span> ({formattedPrice})</span>
+							</div>
+							<div className='cart-item-actions'>
+								{/* <button onClick={() => handleRemoveFromCart(item.id)}>
                     -
-                  </button>
-                  <span>{item.quantity}</span>
-                  <button onClick={() => handleAddToCart(item)}>+</button>
-                </div>
-              </li>
-            );
-          })}
-        </ul> */}
+                  </button> */}
+								<span>{item.quantity}</span>
+								{/* <button onClick={() => handleAddToCart(item)}>+</button> */}
+							</div>
+						</li>
+					);
+				})}
+			</ul>
 
 			{/* <p id="cart-total-price">
         Cart Total: <strong>{formattedTotalPrice}</strong>
